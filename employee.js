af537f6033703677ab2e3ccab721bf1c141051ad
Katie Thorpe <kthorpe1023@gmail.com>
2020-06-21 14:54:53 -0600

    inquirer menu working

--- employee.js
+++ employee.js
@@ -12,7 +12,35 @@ const connection = mysql.createConnection({
     database: "employee_tracker_db"
 });
 
+const viewEmployees = () => {
+    connection.query("SELECT * FROM department", (err, data) =>{
+        if(err) throw err;
+        for(const items of data){
+            console.log(`ID. ${items.id} ${items.name}`)
+        }
+    })
+}
+
+const menu = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add employee", "Remomve employee", "Update employee role", "Update employee manager"],
+            name: "action",
+        }
+    ]).then((answer) => {
+        switch(answer.action) {
+            case "View all employees":
+                return viewEmployees();
+            case "View all employees by department":
+                return employeesDepartment();
+        }
+    });
+};
+
 connection.connect((err) => {
     if(err) throw err;
-    console.log(`Connected to id ${connection.threadId}`)
+    console.log(`Connected to id ${connection.threadId}`);
+    menu();
 });
