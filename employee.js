ea6d82a71e69377175573f3cea211c8e08360afa
Katie Thorpe <kthorpe1023@gmail.com>
2020-06-22 09:05:51 -0600

    first attempt remove employee (not working)

--- employee.js
+++ employee.js
@@ -12,14 +12,87 @@ const connection = mysql.createConnection({
     database: "employee_tracker_db"
 });
 
+var query = "SELECT employee.id, employee.first_name, employee.last_name, department.name, role.title, employee.manager, role.salary";
+query += "FROM department INNER JOIN role INNER JOIN employee ON (department.id = role.department_id AND employee.department_id);";
+
 const viewEmployees = () => {
-    connection.query("SELECT * FROM department", (err, data) =>{
+    connection.query(query, (err, data) =>{
         if(err) throw err;
+        
         for(const items of data){
-            console.log(`ID. ${items.id} ${items.name}`)
+            console.log(`ID. ${items.id} || Name: ${items.first_name} ${items.last_name} || Department: ${items.name} || Title: ${items.title} || Manager: ${items.manager} || Salary: ${items.salary}`)
         }
     })
 }
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the first name of this employee?",
+            name: "first_name",
+           //add validate: function(value) if possible to ensure string and NaN
+        },
+        {
+            type: "input",
+            message: "What is the last name of this employee?",
+            name: "last_name",
+            //same validate as above
+        },
+        {
+            type: "list",
+            message: "Who is this employee's manager?",
+            name: "manager",
+            choices: ["Troy Barnes", "Abed Nadir", "Britta Perry", "Annie Edison", "Shirley Bennett", "Jeff Winger"]
+
+        },
+        {
+            type: "list",
+            message: "What is the employee's title?",
+            name: "title",
+            choices: ["marketing coordinator", "receptionist", "web developer", "managmr"]
+
+        }
+
+    ]).then((data) => {
+        connection.query("INSERT INTO employee SET ?",
+        {
+            first_name: data.first_name,
+            last_name: data.last_name,
+            manager: data.manager,
+            title: data.title,
+        },
+        (err)=> {
+            if(err) throw err;
+            console.log(`Employee ${data.first_name} has been added`)
+        })
+    })
+};
+
+
+const removeEmployee = () => {
+    // let empName = []
+    // const nameTime = () => {connection.query("SELECT first_name, last_name FROM employee;", (err, res) => {
+    //     if(err) throw err;
+    //     for(items of res){
+    //         empName.push(`${items.first_name} ${items.last_name}`)
+    //     }
+    // })
+    // }
+    // nameTime()
+    // console.log(empName);
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Which employee would you like to remove?",
+            name: "employeeName",
+            choices: [empName]
+        }
+    ]).then((data) =>{
+        connection.query("DELETE FROM employee WHERE first_name = ? AND last_name = ?",
+        [first_name = data.first_name],[last_name = data.last_name])
+    })
+};
 
 const menu = () => {
     inquirer.prompt([
@@ -33,8 +106,25 @@ const menu = () => {
         switch(answer.action) {
             case "View all employees":
                 return viewEmployees();
+                break;
             case "View all employees by department":
                 return employeesDepartment();
+                break;
+            case "View all employees by manager":
+                return employeeManager();
+                break;
+            case "Add employee":
+                return addEmployee();
+                break;
+            case "Remomve employee":
+                return removeEmployee();
+                break;
+            case "Update employee role":
+                return updateRole();
+                break;
+            case "Update employee manager":
+                return updateManager();
+                break;
         }
     });
 };
--- schema.sql
+++ schema.sql
@@ -24,7 +24,21 @@ CREATE TABLE employee(
 id INT AUTO_INCREMENT NOT NULL,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
-role_id INT NOT NULL,
-manager_id INT,
+manager VARCHAR(30) NOT NULL,
+title VARCHAR(30) NOT NULL,
 primary key(id)
 );
+
+INSERT INTO employee(first_name, last_name, manager_id) VALUES("Betsy", "Johnson", 8675);
+INSERT INTO department(name) VALUES("marketing");
+INSERT INTO department(name) VALUES("reception");
+INSERT INTO department(name) VALUES("web development");
+INSERT INTO department(name) VALUES("manager");
+
+INSERT INTO role(title, salary, department_id) VALUES("marketing coordinator", 50000, 1);
+INSERT INTO role(title, salary, department_id) VALUES("receptionist", 50000, 2);
+INSERT INTO role(title, salary, department_id) VALUES("web developer", 70000, 3);
+INSERT INTO role(title, salary, department_id) VALUES("manager", 80000, 4);
+
+SELECT department.id, department.name, role.title, role.salary
+FROM department INNER JOIN role INNER JOIN employee ON (department.id = role.department_id AND role.title=employee.title);
